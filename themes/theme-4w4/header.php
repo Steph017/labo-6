4ac21c4d72828c879f8e86abfbafae7a7645dccf
Stephanie Plante <e1821985@cmaisonneuve.qc.ca>
2021-02-23 16:23:27 -0500

    ajout carrousel dans le header de page acceuil.

--- themes/theme-4w4/header.php
+++ themes/theme-4w4/header.php
@@ -45,6 +45,20 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
+		<!-- Debut du carrousel -->
+		<?php
+		if ( is_front_page()) : ?>
+		<section class="carrousel">
+				<div>1</div>
+				<div>2</div>
+				<div>3</div>
+		</section>
+		<button id='un'>1</button>
+		<button id='deux'>2</button>
+		<button id='trois'>3</button>
+		<?php endif ?>	
+
+
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><img src="https://s2.svgbox.net/hero-solid.svg?ic=menu&color=000000" width="32" height="32"></button>
 			<?php
